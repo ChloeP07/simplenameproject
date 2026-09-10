26d7d7a8796e64ffc43358b8df09855edbf13469
Serene Davis <serenewdavis@gmail.com>
2026-09-10 16:06:30 -0500

    Update main.dart
    made front bigger

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,13 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
-            const Text('Serene has pushed the button this many times:'),
+            const Text(
+              'Serene has pushed the button this many times:',
+              style: TextStyle(
+                fontSize: 124.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
